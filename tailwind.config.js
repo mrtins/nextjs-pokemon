e29ec8ab90106e19b3655e5bce1948a3ada80ec8
Vitor Martins <vitorfigmartins@gmail.com>
2022-04-17 18:33:36 +0100

    Setting up project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
